a163c64354d60845ddc8d7f5e35c9af4b2511147
sherry-du <726845321@qq.com>
2018-07-04 20:42:10 +0800

    ex00

---
+++ ex00/Skat.cpp
@@ -0,0 +1,57 @@
+#include "Skat.h"
+
+Skat::Skat(std::string const& name, int stimPaks)
+{
+    this->_name = name;
+    this->_stimPaks = stimPaks;
+}
+
+Skat::~Skat(){}
+
+int Skat::stimPaks()
+{
+    return this->_stimPaks;
+}
+
+const std::string& Skat::name()
+{
+    return this->_name;
+}
+
+void Skat::shareStimPaks(int number, int stock)
+{
+    if(this->_stimPaks >= number)
+    {
+	stock += number;
+	this->_stimPaks -= number;
+	std::cout<<"Keep the change."<<std::endl; 
+    }
+    else
+	std::cout<<"Don't be greedy"<<std::endl;
+}
+
+void Skat::addStimPaks(unsigned int number)
+{
+    if(number == 0)
+	std::cout<<"Hey boya, did you forget something ?"<<std::endl;
+    else
+	this->_stimPaks += number;
+}
+
+void Skat::useStimPaks()
+{
+    if(this->_stimPaks > 0)
+    {
+	this->_stimPaks -= 1;
+	std::cout<<"Time to kick some ass and chew bubble gum."<<std::endl;
+    }
+    else
+	std::cout<<"Mediiiiiic"<<std::endl;
+}
+
+void Skat::status()
+{
+    std::cout<<"Soldier "<<this->name()<<" reporting "<<this->stimPaks()<<" stimpaks remaining sir !"<<std::endl;
+}
+
+
---
+++ ex00/Skat.h
@@ -0,0 +1,28 @@
+#ifndef _SKAT_
+#define _SKAT_
+
+#include <iostream>
+
+class Skat
+{
+    public:
+	Skat(std::string const& name = "bob", int stimPaks = 15);
+	~Skat();
+    public:
+	int stimPaks();
+	const std::string& name();
+    public:
+	void shareStimPaks(int number, int stock);
+	void addStimPaks(unsigned int number);
+	void useStimPaks();
+    public:
+	void status();
+    private:
+	std::string _name;
+	int _stimPaks;
+};
+
+
+#endif /* _SKAT_ */
+
+
